12f678e471683c741758f2072546f5990e91d61e
ayoubfatah <ayoubfatah222@gmail.com>
2024-05-18 22:42:26 +0100

    making the login better XD

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,14 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+    const queryClient = useQueryClient()
     const navigate = useNavigate();
     const { mutate, isLoading } = useMutation({
         mutationFn: ({ email, password }) => login({ email, password }),
         onSuccess: (result) => {
             if (result.success) {
+                queryClient.setDefaultOptions(["user"] , result?.data)
                 navigate("/dashboard");
             } else {
                 toast.error("Provided email or password are incorrect");

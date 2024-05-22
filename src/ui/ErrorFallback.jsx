1a6c72f39dcea6837d4eefd9ed65206db8d0bdc9
ayoubfatah <ayoubfatah222@gmail.com>
2024-05-22 22:24:18 +0100

    adding the react error boundary package

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Heading from "./Heading";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +30,17 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+
+
+
+
+export default function ErrorFallback({error}) {
+  return (
+    <StyledErrorFallback>
+      <Box>
+        <Heading as="h1">Something went wrong </Heading>
+      </Box>
+    </StyledErrorFallback>
+  )
+}
